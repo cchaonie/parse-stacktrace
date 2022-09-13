920c04bd8fc0c61dbe4ab1832c33b9d7ab14cd25
Chris Nie <cnie@travix.com>
2022-09-13 19:17:03 +0800

    Remove useless stuff

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -8,7 +8,7 @@ export default () => {
   const [status, setStatus] = useState<LoadingStatus>(LoadingStatus.Loading);
   return (
     <SyncDocument onStatusChange={status => setStatus(status)}>
-      <div className='bg-black-100' id='board'>
+      <div className='background' id='board'>
         <Editor status={status} />
       </div>
     </SyncDocument>
