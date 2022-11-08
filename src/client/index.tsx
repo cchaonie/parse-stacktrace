1f5ac8b858183c8d47a6ecd3798b0f94dfe63926
Chris Nie <nc_8093@163.com>
2022-11-08 20:15:42 +0800

    Add iconfont

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import './index.css';
+
 import './models/core/initializeShareDBDocument';
 
 createRoot(document.querySelector('#root')).render(
