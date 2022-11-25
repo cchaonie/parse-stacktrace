d4d06261d749676d184b24d5d2c58426e81ace80
cchaonie <nc_8093@163.com>
2022-11-25 03:03:34 +0000

    Add italic

--- src/client/component/Toolbar/Toolbar.tsx
+++ src/client/component/Toolbar/Toolbar.tsx
@@ -6,19 +6,27 @@ import styles from './toolbar.css';
 export default () => {
   const editor = useSlate();
 
-  const handleBold = useCallback(() => {
-    Editor.addMark(editor, 'bold', true);
-  }, [editor]);
+  const handleStyle = useCallback(
+    (style: string) => {
+      Editor.addMark(editor, style, true);
+    },
+    [editor]
+  );
 
   return (
     <div className={styles['toolbar']}>
       <div
-        onClick={handleBold}
+        onClick={() => handleStyle('bold')}
         className={`${styles['toolbarItem']} ${styles['bold']}`}
       >
         B
       </div>
-      <div className={`${styles['toolbarItem']} ${styles['italic']}`}>I</div>
+      <div
+        onClick={() => handleStyle('italic')}
+        className={`${styles['toolbarItem']} ${styles['italic']}`}
+      >
+        I
+      </div>
     </div>
   );
 };
